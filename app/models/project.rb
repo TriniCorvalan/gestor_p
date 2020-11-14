128c530c4a827ff1fbf1a56bd91394c24cedb6ef
Trini <trini.4595@gmail.com>
2020-11-14 17:15:27 -0300

    validates

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,16 +1,16 @@
 class Project < ApplicationRecord
     # attribute :locale, :string, default: 'en'
-    # validates :name, :description, presence: true
-    # enum status: %w[propuesta en_progreso terminada]
+    validates :name, :description, presence: true
+    enum status: %w[propuesta en_progreso terminada]
     
     
-    # before_save :has_status
+    before_save :has_status
 
-    # def has_status
-    #     if self.status.present?
-    #         self.status.save
-    #     else
-    #         self.status = "propuesta"
-    #     end
-    # end
+    def has_status
+        if self.status.present?
+            self.status.save
+        else
+            self.status = "propuesta"
+        end
+    end
 end
